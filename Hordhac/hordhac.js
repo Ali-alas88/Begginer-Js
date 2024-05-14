173804467fe8b2b1389ed39bc5a30755d2140b0a
Coder-Ali <alialazmoha292@gmail.com>
2024-05-14 18:36:52 +0300

    commit for the folders

---
+++ Hordhac/hordhac.js
@@ -0,0 +1,19 @@
+
+// Waa 4-ta Qaab Ugu Caansan ee  Wax losooo Bandhigo 
+
+ 
+// Alert Message 
+alert("Look at the console ");
+ 
+// Prompt Message 
+prompt("Ma Fiirin Miyaa Wili ? 😒 ");
+
+// Confirm Message 
+confirm("Hada Fiiri Waa yahee 😍")
+
+// Console Output Message 
+console.log("Waad Fiirise  Console-Ka Waad Mahadsantahay");
+
+
+
+
---
+++ Variables/variables.js
@@ -0,0 +1,18 @@
+var x = 10;
+var k = 20;
+
+console.log(x); // Output: 10
+console.log(x + k); // Output: 30
+
+
+let y = 20;
+let m = 40;
+console.log(y); // Output: 20
+console.log(y + m); // Output: 60
+
+
+
+const z = 30;
+const n = 80;
+console.log(z); // Output: 30
+console.log(z + n); // Output: 110
